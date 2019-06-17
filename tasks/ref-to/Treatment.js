c53fc4120b3f9fca15826aa25a10fcc092603fe3
Rodolfo Meza <copelojr@gmail.com>
2019-06-17 09:12:17 -0500

    Refactor: Create react functional component for treatment option

---
+++ tasks/ref-to/Treatment.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+const Treatment = (props) => {
+    let index = props.index;
+    let value = props.value;
+    let str = props.str;
+  return (
+    <div
+        className="subtreatmenthr custom1-subtreatmenthr treatmentclshome"
+        key={index}
+    >
+        <span className="treatment-outer1">
+        <span className="treatement-images">
+            <img
+            src={value.iconUrl}
+            className={
+                value.className ? value.className : "imglogosize"
+            }
+            />{" "}
+        </span>{" "}
+        <span className="treatmentoptionhomecount">
+            {" "}
+            {value.count}{" "}
+        </span>{" "}
+        <span className="treatmentoptionhomename">
+            {" "}
+            {value.name}{" "}
+        </span>
+        </span>
+        <Link
+        to={`/treatment-option/treatments/${str}`}
+        className="treatment-outer-btn"
+        >
+        <button className="btn-rt btn blue_btn text-center hvr-bounce-to-top treatmentcls">
+            {" "}
+            {value.btnlabel
+            ? value.btnlabel
+            : "View Treatments"}{" "}
+        </button>{" "}
+        </Link>
+    </div>
+  );
+};
+
+export default Treatment;
--- tasks/ref-to/index.js
+++ tasks/ref-to/index.js
@@ -20,6 +20,7 @@ import ThingsToConsider from "./ThingsToConsider";
 import TreatmentName from "./treatmentName";
 import NoTreatmentMessageOptions from "./NoTreatmentMessageOptions";
 import TreatmentMessageOptions from "./TreatmentMessageOptions";
+import Treatment from "./Treatment";
 
 class TreatmentOptions extends Component {
   constructor(props) {
@@ -496,40 +497,7 @@ class TreatmentOptions extends Component {
                 this.state.legendsval.map((value, index) => {
                   str = value.name.replace(/\s+/g, "-").toLowerCase();
                   return (
-                    <div
-                      className="subtreatmenthr custom1-subtreatmenthr treatmentclshome"
-                      key={index}
-                    >
-                      <span className="treatment-outer1">
-                        <span className="treatement-images">
-                          <img
-                            src={value.iconUrl}
-                            className={
-                              value.className ? value.className : "imglogosize"
-                            }
-                          />{" "}
-                        </span>{" "}
-                        <span className="treatmentoptionhomecount">
-                          {" "}
-                          {value.count}{" "}
-                        </span>{" "}
-                        <span className="treatmentoptionhomename">
-                          {" "}
-                          {value.name}{" "}
-                        </span>
-                      </span>
-                      <Link
-                        to={`/treatment-option/treatments/${str}`}
-                        className="treatment-outer-btn"
-                      >
-                        <button className="btn-rt btn blue_btn text-center hvr-bounce-to-top treatmentcls">
-                          {" "}
-                          {value.btnlabel
-                            ? value.btnlabel
-                            : "View Treatments"}{" "}
-                        </button>{" "}
-                      </Link>
-                    </div>
+                    <Treatment str={str} index={index} value={value}/>
                   );
                 })}
             </div>
